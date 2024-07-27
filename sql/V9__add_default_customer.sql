43642d4e49150961918f6b624cb82c8c03c7b005
Felipe Tomm <felipe.tomm@gmail.com>
2024-07-27 18:49:13 -0300

    issue: #1 description: - Add sql files to apply migrations.

---
+++ sql/V9__add_default_customer.sql
@@ -0,0 +1,15 @@
+insert into customer (
+	cpf,
+	name,
+	email,
+	phone,
+	created_at,
+	updated_at
+) values (
+	'00000000019',
+	'Não Identificado',
+	'burguer-adm@burguer.com',
+	null,
+	'2024-05-01 00:00:00.000',
+	'2024-05-01 00:00:00.000'
+);
