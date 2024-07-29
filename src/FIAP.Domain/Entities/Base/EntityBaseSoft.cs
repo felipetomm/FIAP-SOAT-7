7cc4396e5745ef8746024c405032b22480dc4f8a
Felipe Tomm <felipe.tomm@gmail.com>
2024-07-28 23:33:25 -0300

    issue: #1 description: - Adjust entity base soft.

--- src/FIAP.Domain/Entities/Base/EntityBaseSoft.cs
+++ src/FIAP.Domain/Entities/Base/EntityBaseSoft.cs
@@ -6,10 +6,6 @@ public abstract class EntityBaseSoft<TId> : EntityBase<TId>
 
     public virtual DateTime? Modified { get; set; }
 
-    public virtual long CreatedBy { get; set; }
-
-    public virtual long? ModifiedBy { get; set; }
-
     public virtual Guid Hash { get; set; }
 
     public virtual bool Deleted { get; set; }
