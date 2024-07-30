518b4c6e1c9b926c6cf9fb80b9abfcd2d7ca2d70
Felipe Tomm <felipe.tomm@gmail.com>
2024-07-30 18:38:36 -0300

    issue: #1 description: - Add controllers models.

---
+++ src/FIAP.Application/InputModels/CreateCustomerModel.cs
@@ -0,0 +1,9 @@
+namespace FIAP.Application.InputModels;
+
+public class CreateCustomerModel
+{
+    public string Name { get; set; }
+    public string Cpf { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
---
+++ src/FIAP.Application/InputModels/UpdateOrderModel.cs
@@ -0,0 +1,7 @@
+namespace FIAP.Application.InputModels;
+
+public class UpdateOrderModel
+{
+    public long Id { get; set; }
+    public string Status { get; set; }
+}
