0e32c023dd4c78522715bc97ecaf72712f50b80c
Felipe Tomm <felipe.tomm@gmail.com>
2024-07-30 18:38:20 -0300

    issue: #1 description: - Fix IsEmpty extension method.

--- src/FIAP.Infrastructure.CrossCutting/Extensions/String.String.Extension.cs
+++ src/FIAP.Infrastructure.CrossCutting/Extensions/String.String.Extension.cs
@@ -2,7 +2,7 @@ namespace FIAP.Infrastructure.CrossCutting.Extensions;
 
 public static class StringStringExtension
 {
-    public static bool IsEmpty(this string @this)
+    public static bool IsEmpty(this string @this, bool numberOnly = false)
     {
         if (string.IsNullOrWhiteSpace(@this))
             return true;
@@ -10,12 +10,15 @@ public static class StringStringExtension
         if (string.IsNullOrEmpty(@this))
             return true;
 
-        var numbersStr = @this.OnlyDigits();
-        if (string.IsNullOrWhiteSpace(numbersStr))
-            return true;
+        if (numberOnly)
+        {
+            var numbersStr = @this.OnlyDigits();
+            if (string.IsNullOrWhiteSpace(numbersStr))
+                return true;
 
-        if (numbersStr.ConvertValue<long>() == 0)
-            return true;
+            if (numbersStr.ConvertValue<long>() == 0)
+                return true;
+        }
 
         return false;
     }
