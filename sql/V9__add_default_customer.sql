a4412ee5f74d66d0535950a6fc0e3836f1dd6bf9
Felipe Tomm <felipe.tomm@gmail.com>
2024-10-15 22:49:38 -0300

    issue: #8 description: - Correção uuid do customer padrão

--- sql/V9__add_default_customer.sql
+++ sql/V9__add_default_customer.sql
@@ -1,15 +1,20 @@
-insert into customers (
-	cpf,
-	name,
-	email,
-	phone,
-	created,
-	modified
-) values (
-	'00000000019',
-	'Não Identificado',
-	'burguer-adm@burguer.com',
-	null,
-	'2024-05-01 00:00:00.000',
-	'2024-05-01 00:00:00.000'
-);
+INSERT INTO
+    customers (
+        cpf,
+        name,
+        email,
+        phone,
+        created,
+        modified,
+        hash
+    )
+VALUES
+    (
+        '00000000019',
+        'Não Identificado',
+        'burguer-adm@burguer.com',
+        NULL,
+        '2024-05-01 00:00:00.000',
+        '2024-05-01 00:00:00.000',
+        gen_random_uuid()
+    );
